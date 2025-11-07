ecb537dae81162c7abc34f624d60c40be2f203e9
Jian Cui <cuijian2015@qq.com>
2025-11-07 11:47:43 +0800

    update infer sh.

--- infer_mage.sh
+++ infer_mage.sh
@@ -1,1 +1,7 @@
+record_dir=/home/cj/.cache/huggingface/lerobot/jiancui/mage-infer
+if [ -d "$record_dir" ]; then
+    echo "目录存在，正在删除: $record_dir"
+    rm -rf "$record_dir"
+    echo "目录已成功删除"
+fi
 CUDA_VISIBLE_DEVICES=1 python -m src.lerobot.robots.robot_car.evaluate 
